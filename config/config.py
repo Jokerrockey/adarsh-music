43135d28510984d0277be5493dd52abcd931f537
Jokerrockey <101962378+Jokerrockey@users.noreply.github.com>
2022-07-03 22:30:48 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -36,8 +36,8 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AnonymousR1025/AnonXMusic", "https://github.com/Jokerrockey/lkfpvtrepo"
-)
+    "https://github.com/AnonymousR1025/AnonXMusic",) 
+
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
 GIT_TOKEN = getenv("GIT_TOKEN", None)
